9c3dcd925b1bee14c6fa6668f6844339e0e2ba42
Jack Davis <jdavis@trioxis.com>
2017-05-15 22:51:06 +1000

    Add swanky exampe

--- docs/src/examples/basicForm.js
+++ docs/src/examples/basicForm.js
@@ -2,32 +2,166 @@ import React from 'react';
 import { LocalStateForm } from 'immutable-react-form';
 import { Map } from 'immutable';
 
-const formData = {
-  name:'Derik Zoolander'
+import MUITextField from 'material-ui/TextField';
+import MUIPaper from 'material-ui/Paper';
+import MUIFlatButton from 'material-ui/FlatButton';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+
+import s from './basicForm.css';
+
+let formData = {
+  user:{
+    name:'',
+    shippingAddress:'',
+    username:''
+  },
+  cart:[{
+    quantity:5,
+    item:'Bertie Botts Every Flavour Beans'
+  }, {
+    quantity:1,
+    item:'Cauldron'
+  }, {
+    quantity:1,
+    item:'Nimbus 2000'
+  }, {
+    quantity:3,
+    item:'Robe'
+  }]
 }
 
-
 function Example(props){
-  const {form} = props;
+  const {
+    form,
+    form:{
+      model
+    }
+  } = props;
+
   return <div>
-    <label>
-      Name
-    </label>
-    <input
-      type='text'
-      value={form.model.get('name')}
-      onChange={(e)=>
-        form.update(form.model.set('name',e.target.value))
-      }
-    />
-    <p>
-      Hello, my name is {form.model.get('name')}
-    </p>
+    <MUIPaper className={s.formPaper}>
+      <form onSubmit={form.submission.submit}>
+        <TextField
+          floatingLabelText="Name"
+          fullWidth
+          type='text'
+          form={form}
+          field='user.name'
+        />
+        <TextField
+          floatingLabelText="Shipping Address"
+          fullWidth
+          type='text'
+          form={form}
+          field='user.shippingAddress'
+        />
+        <TextField
+          floatingLabelText="Username (create new user)"
+          fullWidth
+          type='text'
+          form={form}
+          field='user.username'
+        />
+        <MUIPaper className={s.cart}>
+          {model.get('cart').map((item,i)=>(
+            <CartItem
+              item={item}
+              key={i}
+              onChange={newItem=>form.update(
+                model.setIn(['cart',i],newItem)
+              )}
+              onDelete={()=>form.update(
+                model.deleteIn(['cart',i])
+              )}
+            />
+          ))}
+          <FloatingActionButton
+            mini secondary
+            className={s.cartAddButton}
+            onClick={e => form.update(
+              model.update(
+                'cart',
+                list=>list.push(NewCartItem())
+              )
+            )}
+            >
+            <ContentAdd />
+          </FloatingActionButton>
+        </MUIPaper>
+
+        <MUIFlatButton
+          primary
+          onClick={form.submission.submit}>
+          Continue to "Payment"
+        </MUIFlatButton>
+      </form>
+    </MUIPaper>
+    <MUIPaper className={s.formPaper}>
+      <h3>Raw Data</h3>
+      {JSON.stringify(model)}
+    </MUIPaper>
   </div>
 }
 
 export default LocalStateForm(
   props => (formData),
   null,
-  (model, props) => console.log('Submitted!')
+  (model, props) => {
+    formData = model.toJS()
+  }
 )(Example);
+
+function CartItem(props){
+  const {
+    onChange,
+    onDelete,
+    item
+  } = props;
+  return <div className={s.cartItem}>
+    <input
+      type='text'
+      className={s.cartItemName}
+      value={item.get('item')}
+      onChange={e=>onChange(item.set('item',e.target.value))}
+    />
+    <input
+      type="number" min="1" max="10" step='1'
+      value={item.get('quantity')}
+      onChange={e=>onChange(item.set('quantity',e.target.value))}
+    />
+    <button onClick={e => onDelete()}>
+      X
+    </button>
+  </div>
+}
+
+function TextField(props){
+  const {
+    form,
+    field,
+    ...remainingProps
+  } = props;
+
+  return <MUITextField
+    {...remainingProps}
+    value={form.model.getIn(field.split('.'))}
+    onChange={(e)=>
+      form.update(form.model.setIn(field.split('.'),e.target.value))
+    }
+  />
+}
+
+function GetStockNames(){
+  return [
+    'Bertie Botts Every Flavour Beans',
+    'Cauldron',
+    'Nimbus 2000',
+    'Robe',
+    'Invisible Cloak'
+  ];
+}
+
+function NewCartItem(){
+  return new Map({item:'',quantity:1});
+}
--- docs/src/index.js
+++ docs/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import s from './style.css';
 
@@ -10,36 +13,53 @@ import ExampleThree from './exampleThree';
 import ExampleValidation from './exampleValidation';
 import ExampleValidationTwo from './exampleValidationTwo';
 
-function MainComponent(props){
-  return <div>
-    <h1>
-      Immutable React Form
-      <small>
-        Form state management
-      </small>
-    </h1>
-    <ul>
-      <li>No involvement of rendering</li>
-      <li>Opinionated form API</li>
-      <li>Immutable form data</li>
-      <li>HOC, not components</li>
-    </ul>
 
-    <ExampleHolster code={BasicFormExampleCode}>
-      <h2>
-        Simple state management
-      </h2>
-      <BasicFormExample />
-    </ExampleHolster>
-    <ExampleTwo />
-    <ExampleThree />
-    <ExampleValidation />
-    <ExampleValidationTwo />
-  </div>
+injectTapEventPlugin();
+
+function Docs(props){
+  return <MuiThemeProvider>
+    <div>
+      <h1>
+        Immutable React Form
+        <small>
+          Form state management
+        </small>
+      </h1>
+      <ul>
+        <li>No involvement of rendering</li>
+        <li>Opinionated form API</li>
+        <li>Immutable form data</li>
+        <li>HOC, not components</li>
+      </ul>
+
+      <ExampleHolster code={BasicFormExampleCode}>
+        <h2>
+          Shopping Cart for Hogwarts Example
+        </h2>
+        <p>
+          Immutable React Form used at it's full potential! Features:
+        </p>
+        <ul>
+          <li>Form model with nested fields</li>
+          <li>And arrays!</li>
+          <li>Custom text fields with Material UI</li>
+          <li>Validation to confirm all fields are entered</li>
+          <li>Async Validation on Username (to check against "server")</li>
+          <li>Reset form fields</li>
+          <li>Async submission</li>
+        </ul>
+        <BasicFormExample />
+      </ExampleHolster>
+      <ExampleTwo />
+      <ExampleThree />
+      <ExampleValidation />
+      <ExampleValidationTwo />
+    </div>
+  </MuiThemeProvider>
 }
 
 ReactDOM.render(
-  <MainComponent />,
+  <Docs />,
   document.getElementById('root')
 );
 
