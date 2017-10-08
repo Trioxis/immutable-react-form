7dfb28b363819cff2e2452f1be981d44e41db619
Jack Davis <jdavis@trioxis.com>
2017-10-08 12:41:53 +1100

    Add test to prove data may be calculated from props

--- src/__tests__/higherOrderComponent.test.js
+++ src/__tests__/higherOrderComponent.test.js
@@ -19,3 +19,18 @@ test('Enhanced component should render',()=>{
   expect(tree.contains(formElements)).toBe(true)
   expect(tree).toMatchSnapshot()
 })
+
+test('Form data may be calculated from props',()=>{
+  const MyForm = (props)=><span>{props.form.model.get('thing')}</span>;
+
+  const EnhancedForm = injectForm(
+    props=>props.data,
+    props=>null,
+    props=>({})
+  )(MyForm)
+
+  const tree = mount(<EnhancedForm data={{thing:'Wow'}} />);
+
+  expect(tree.contains(<span>Wow</span>)).toBe(true)
+  expect(tree).toMatchSnapshot()
+})
